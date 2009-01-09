25d2bb27525b6c6148ed01152e40bcae62a92957
wirawan <wirawan>
2009-01-09 21:48:59 +0000

    * Minor upgrades for conciseness and (hopefully) efficiency.

--- multidet.py
+++ multidet.py
@@ -1,4 +1,4 @@
-# $Id: multidet.py,v 1.1 2008-10-02 04:06:25 wirawan Exp $
+# $Id: multidet.py,v 1.2 2009-01-09 21:48:59 wirawan Exp $
 #
 # multidet.py
 # Created: 20080925
@@ -8,9 +8,14 @@
 # Unfortunately what looks nice in octave has its own cost: time.
 # Hopefully this is not the case with python/scipy.
 
-import scipy
-import scipy.linalg
-from numpy import matrix, conj
+#import scipy
+#import scipy.linalg
+from numpy import abs as Abs
+# scipy is too sweeping, we will revert to numpy instead:
+#from scipy import array, asmatrix, asarray, conj, matrix, ndarray, sqrt
+#from scipy.linalg import det
+from numpy import array, asmatrix, asarray, conj, matrix, ndarray, sqrt
+#from numpy.lib.scimath import sqrt
 from numpy.linalg import det
 
 class Det(object):
@@ -21,12 +26,12 @@ class Det(object):
     if src_up != None:
       self.make(src_up, src_dn, ndn)
   def norm(self):
-    return det( self.up.H * self.up ) \
-         * det( self.dn.H * self.dn )
+    return (det(self.up.H * self.up) \
+            * det(self.dn.H * self.dn)) # .real
   def make(self, src_up, src_dn = None, ndn = None):
-    self.up = matrix(src_up)
+    self.up = asmatrix(array(src_up,order='C'))
     if (src_dn != None):
-      self.dn = matrix(src_dn)
+      self.dn = asmatrix(array(src_dn,order='C'))
     elif (ndn != None):
       self.dn = self.up[:, 0:ndn]
     else:
@@ -41,8 +46,39 @@ class Det(object):
     return self.up.shape[0]
 
 class MultiDet(object):
-  ampl = []
-  det = []
+  # Contains:
+  #dets = []
+  # where det is a list of Det objects, with attached "ampl"
+  # field in it.
+  def set_ampl(self,mtx):
+    '''Sets the determinant amplitudes. The mtx argument is either a numpy
+    array, or python list or tuple.'''
+    if isinstance(mtx, ndarray):
+      for (d, ampl) in zip(self.dets, asarray(mtx).reshape(len(self.dets))):
+        d.ampl = ampl
+    elif isinstance(mtx, list) or isinstance(mtx, tuple):
+      for (d, ampl) in zip(self.dets, mtx):
+        d.ampl = ampl
+  def make_ci(self, ampl, cfg, orbs, nup, ndn):
+    '''Creates a multideterminant wfn by using CI-like configuration.
+    Arguments:
+    - ampl: a list of amplitudes, either as 1-dimensional array/matrix or
+      Python list.
+    - cfg: list of determinant configurations.
+    - orbs: a list of single-particle orbitals.
+    '''
+    #ndets =
+    self.set_ampl(ampl)
+  def norm(self):
+    return mdet_ovlp(self)
+  def normalize(self, N = None):
+    if (N != None):
+      n = Abs(N)
+    else:
+      n = mdet_ovlp(self)
+    self.norm_orig = n
+    for d in self.dets: d.ampl *= 1.0 / sqrt(n)
+
 
 # Functions:
 
@@ -63,23 +99,19 @@ def det_ovlp(det1, det2): #{
 def mdet_ovlp(mdet1, mdet2 = None): #{
   '''Computes overlap between two MultiDet objects'''
   ovlp = 0 + 0j
-  Lcnt = len(mdet1.ampl)
   if (mdet2 == None):
     L = 0
-    for L in xrange(0, Lcnt):
-      for R in xrange(L, Lcnt):
-        ovlp1 = conj(mdet1.ampl[L]) * mdet1.ampl[R] \
-              * det_ovlp(mdet1.det[L], mdet1.det[R])
-        if (L == R):
-          ovlp += ovlp1
-        else:
-          ovlp += conj(ovlp1) + ovlp1
+    for Ldet in mdet1.dets:
+      ovlp += abs(Ldet.ampl)**2 * Ldet.norm()
+      L += 1
+      for Rdet in mdet1.dets[L:]:
+        ovlp += 2 * (conj(Ldet.ampl) * Rdet.ampl \
+                     * det_ovlp(Ldet, Rdet)).real
   else:
-    Rcnt = len(mdet2.ampl)
-    for L in xrange(0, Lcnt):
-      for R in xrange(0, Rcnt):
-        ovlp += conj(mdet1.ampl[L]) * mdet1.ampl[R] \
-              * det_ovlp(mdet1.det[L], mdet1.det[R])
+    for Ldet in mdet1.dets:
+      for Rdet in mdet2.dets:
+        ovlp += conj(Ldet.ampl) * Rdet.ampl \
+              * det_ovlp(Ldet, Rdet)
   return ovlp
 #}mdet_ovlp
 
