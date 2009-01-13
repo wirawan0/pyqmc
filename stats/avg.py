ec02c2d1e3b5fbb48486087177b8324d01b6567b
wirawan <wirawan>
2009-01-13 22:29:13 +0000

    * Simple averaging module. No data is recorded.

---
+++ stats/avg.py
@@ -0,0 +1,27 @@
+# Create: 20090112
+
+import math
+
+class avg(object):
+  def __init__(self):
+    self.clear()
+  def clear(self):
+    self.N = 0
+    self.sum1 = 0.0
+    self.sum2 = 0.0
+  def add(self, value):
+    self.N += 1
+    self.sum1 += value
+    self.sum2 += value*value
+    return self
+  def mean(self):
+    return self.sum1 / self.N
+  def stddev(self):
+    N = self.N
+    avg1 = self.sum1 / N
+    avg2 = self.sum2 / N
+    return math.sqrt(abs( (avg2 - avg1**2) * N / (N - 1) ))
+
+# Operator overloading:
+avg.__iadd__ = avg.add
+avg.__call__ = avg.mean
