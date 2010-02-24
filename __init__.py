13763ce355a9f10a1fda3a2bfd5eb75c891d3f2e
wirawan <wirawan>
2010-02-24 15:22:20 +0000

    * Do not import anything from submodules.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-# $Id: __init__.py,v 1.1 2009-01-09 22:06:11 wirawan Exp $
+# $Id: __init__.py,v 1.2 2010-02-24 15:22:20 wirawan Exp $
 #
 # pyqmc module
 # Created: ~200809
@@ -6,6 +6,7 @@
 #
 # Main pyqmc package. This will import everything.
 
-from pyqmc.multidet import *
-from pyqmc.matrices import *
-from pyqmc.walkers import *
+#from pyqmc.multidet import *
+#from pyqmc.matrices import *
+#from pyqmc.walkers import *
+pass
