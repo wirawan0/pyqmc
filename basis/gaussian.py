06d999fd7ed15dcff95d2c3d9284aa0d1c6b31e2
wirawan <wirawan>
2010-02-24 15:19:58 +0000

    * Added inquiry functions for the summary of the basis (e.g. 6s5p4d2f)   and the number of basis. * Added support for spherical vs cartesian basis types.

--- basis/gaussian.py
+++ basis/gaussian.py
@@ -1,4 +1,4 @@
-# $Id: gaussian.py,v 1.1 2010-02-01 21:29:16 wirawan Exp $
+# $Id: gaussian.py,v 1.2 2010-02-24 15:19:58 wirawan Exp $
 #
 # pyqmc.basis module
 # Created: 20100201
@@ -15,6 +15,15 @@
 
 """
 Module for GTO basis.
+
+----------
+REFERENCES
+----------
+
+[SPHCART]
+  Schlegel & Frisch, Int. J. Quant. Chem. 54, 83-87 (1995)
+
+
 """
 
 import os.path
@@ -31,19 +40,34 @@ class GTOBasis(object):
   """
   Gaussian-type orbital basis.
   """
-  def __init__(self, species, srcfile=None, funcs=[]):
+  # Map function type for packages other than GAMESS
+  map_nwchem_func_type = { 'L': 'SP' }
+  nfuncs_spher_map = {
+    'S': 1, 'P': 3, 'D': 5, 'F': 7, 'G': 9, 'H': 11, 'I': 13,
+    'L': 4,  # special (SP)
+  }
+  nfuncs_cart_map = {
+    'S': 1, 'P': 3, 'D': 6, 'F': 10, 'G': 15, 'H': 21, 'I': 28,
+    'L': 4,  # special (SP)
+  }
+  def __init__(self, species, srcfile=None, funcs=[], ispher=True):
     self.funcs = []
     self.species = species
     if funcs:
       self.funcs = funcs
+      self.ispher = ispher
     elif srcfile:
-      self.load(srcfile)
+      self.load(srcfile, ispher)
     # idea:
     # self.precision_control = GlobalPrecisionControl
     # and by changing local precision_control, we can alter the output
     # below.
 
-  def load(self, srcfile):
+  def load(self, srcfile, ispher=True):
+    """Loads the basis function from a given source file.
+    The data in the source file is essentially in GAMESS (US) format with
+    minor modification to indicate the species and the end of the basis data
+    (a line with only '.' in it)."""
     F = text_input(srcfile)
     try:
       F.seek_text(r'^(?i)spec: *%s$' % (self.species,))
@@ -55,7 +79,8 @@ class GTOBasis(object):
     funcs = []
     while L[0] != '.':
       # Reads a contracted basis
-      func_typ = L[0]
+      # WARNING FIXME: L is still buggy. Still does not read the p coeffs correctly.
+      func_typ = L[0].upper()
       func_gcount = int(L[1])
       func_desc = []
       for i in xrange(0, func_gcount):
@@ -64,6 +89,7 @@ class GTOBasis(object):
       funcs.append((func_typ, func_desc))
       L = F.next_rec()
     self.funcs = funcs
+    self.ispher = ispher
 
   def output_nwchem(self, specname=None, indent=0):
     """Outputs the basis definition in nwchem input format.
@@ -74,13 +100,12 @@ class GTOBasis(object):
 
     return \
       "\n".join([
-        "\n".join(["%s%s  %s" % (indent, specname, typ)] + [
+        "\n".join(["%s%s  %s" % (indent, specname, self.map_nwchem_func_type.get(typ,typ))] + [
             "%s  %17.8f %19.9f" % (indent, Exp, Coeff) \
               for (Exp,Coeff) in desc
         ]) \
           for (typ, desc) in self.funcs
       ])
-
 
   def output_gamess(self, specname=None, indent=0):
     """Outputs the basis definition in gamess(US) input format.
@@ -99,6 +124,30 @@ class GTOBasis(object):
         ]) \
           for (typ, desc) in self.funcs
       ])
+
+  def func_summary(self):
+    """Gives a summary of the basis function, such as `9s8p5d1f'."""
+    funcnames = 'SPDFGHI'
+    count = dict([ (f,0) for f in funcnames ])
+    for (typ, desc) in self.funcs:
+      if typ == 'L':
+        count['S'] += 1
+        count['P'] += 1
+      else:
+        count[typ] += 1
+    return "".join([ "%d%s" % (count[typ], typ.lower()) for typ in funcnames if count[typ] > 0 ])
+
+  def func_count(self, ispher=None):
+    """Gives the number of functions in this basis set.
+    Each magnetic quantum number (for p, d, f, ... -type funcs) counts as
+    a separate function in this formula."""
+    if ispher == None: ispher = self.ispher
+    if ispher:
+      nfunc_map = self.nfuncs_spher_map
+    else:
+      nfunc_map = self.nfuncs_cart_map
+    return sum([ nfunc_map[typ] for (typ, desc) in self.funcs ])
+
 
 
 class GTOBasisAtomList(dict):
@@ -123,16 +172,16 @@ class GTOBasisLib(dict):
   def get_srcfile(self, basis):
     return path_search(self.path, self.fname_map.get(basis, basis) + ".basis")
 
-  def get(self, basis, species, fmt=None, specname=None, indent=0):
+  def get(self, basis, species, fmt=None, specname=None, indent=0, ispher=True):
     if basis not in self:
       self[basis] = GTOBasisAtomList(basis)
 
     List = self[basis]
     if species not in List:
-      List[species] = GTOBasis(species, srcfile=self.get_srcfile(basis))
+      List[species] = GTOBasis(species, srcfile=self.get_srcfile(basis), ispher=ispher)
 
     if fmt == None:
-      return List[species]
+      return List[species] # raw, low-level data
     elif fmt == "nwchem":
       return List[species].output_nwchem(specname=specname, indent=indent)
     elif fmt == "gamess":
@@ -143,3 +192,12 @@ class GTOBasisLib(dict):
 
 # Global library: use this unless you need special customization, please.
 LIB = GTOBasisLib()
+
+
+def sph_norm(func):
+  """This implements Eq.(8) in SPHCART paper, i.e. the normalization
+  constant of a spherical Gaussian function.
+  This will *NOT* do normalization for the 'L' type function"""
+  (typ, desc) = func
+
+
