c509bc233953fccd9a001d012323752a775df1ea
wirawan <wirawan>
2011-06-18 04:14:16 +0000

    * Introducing new pyqmc.matrices module container.   Functions and classes are delegated to submodules for clarity and better   organization.

---
+++ matrices/__init__.py
@@ -0,0 +1,22 @@
+# $Id: __init__.py,v 1.1 2011-06-18 04:14:16 wirawan Exp $
+#
+# pyqmc.matrices module
+# Created: 20080930
+# Changed-to-container-module: 20110617
+# Wirawan Purwanto
+#
+# Standard matrix handling.
+# This module is part of PyQMC project.
+#
+
+"""
+pyqmc.matrices container module
+
+Contains submodules for matrix handling in PyQMC.
+Functions and objects are delegated to submodules to avoid clutter.
+"""
+
+import sys
+import numpy
+
+pass
