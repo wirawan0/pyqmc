904f3b59c9980a2b71492da8ed18631df0676f84
wirawan <wirawan>
2011-08-31 20:14:32 +0000

    * Added pyqmc.sites, for compute-site-specific settings, hooks, and   utilities.

---
+++ sites/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# $Id: __init__.py,v 1.1 2011-08-31 20:14:32 wirawan Exp $
+#
+# pyqmc.sites module
+#
+# Wirawan Purwanto
+# Created: 20110831
+#
+
+"""
+pyqmc.sites
+
+Site-specific support for pyqmc library.
+"""
+
+_sites_supported = [
+  'pmn',
+]
+
+
