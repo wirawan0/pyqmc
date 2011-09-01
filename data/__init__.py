c11d1052bd234bd59c74772671f90d83c0dbd981
wirawan <wirawan>
2011-09-01 20:13:08 +0000

    * pwqmc.data module stub.

---
+++ data/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# $Id: __init__.py,v 1.1 2011-09-01 20:13:08 wirawan Exp $
+#
+# pyqmc.data module
+#
+# Management of QMC data files (except those of calculation results,
+# which are tackled by modules in pwqmc.results).
+#
+# Wirawan Purwanto
+# Created: 20090320
+#
+pass
