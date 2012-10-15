912d47e405deb82d030c8d0883f6fb43ad6061ff
Wirawan Purwanto <wirawan0@gmail.com>
2012-10-15 13:53:57 -0400

    * Added initial CBS extrapolation: Feller's exp() ansatz for SCF energies.

---
+++ basis/cbs/ansatz/__init__.py
@@ -0,0 +1,14 @@
+#
+# pyqmc.basis.cbs.ansatz module
+#
+# Wirawan Purwanto
+# Created: 20121011
+#
+"""
+pyqmc.utils.cbs.ansatz module
+
+Collection of ansatz for use in CBS extrapolation.
+
+"""
+
+pass
